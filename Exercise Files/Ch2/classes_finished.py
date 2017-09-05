e67dd296d93f60b6cc794ad02a77474bad510ad2
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 15:57:09 -0700

    Refactor: classes
    end_of_line

--- Exercise Files/Ch2/classes_finished.py
+++ Exercise Files/Ch2/classes_finished.py
@@ -3,26 +3,31 @@
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
 class myClass():
-  def method1(self):
-    print "myClass method1"
-    
-  def method2(self, someString):
-    print "myClass method2: " + someString
-    
+    lass
+    myClass():
+
+    def method1(self):
+        print("myClass method1")
+
+    def method2(self, someString):
+        print("myClass method2: " + someString)
+
+
 class anotherClass(myClass):
-  def method2(self):
-    print "anotherClass method2"
-    
-  def method1(self):
-    myClass.method1(self);
-    print "anotherClass method1"
-      
-def main():
-  c = myClass()
-  c.method1()
-  c.method2("This is a string")
-  c2 = anotherClass()
-  c2.method1()
-  
+    def method2(self):
+        print("anotherClass method2")
+
+    def method1(self):
+        myClass.method1(self);
+        print("anotherClass method1")
+
+    # exercise the class methods
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
+    c2 = anotherClass()
+    c2.method1()
+
+
 if __name__ == "__main__":
-  main()
+    main()
