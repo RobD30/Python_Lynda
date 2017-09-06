392ffd1a7a1cf5c19477577a005de031edd32f05
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 21:06:42 -0700

    Refactor: appended text file
    end_of_line

--- Exercise Files/Ch4/files_start.py
+++ Exercise Files/Ch4/files_start.py
@@ -2,10 +2,10 @@
 
 def main():
     # Open a file for writing and create it if it doesn't exist
-    f = open("textfile.txt", "w+")
+    # f = open("textfile.txt", "w+")
 
     # Open the file for appending text to the end
-    #  f = open("textfile.txt","a+")
+    f = open("textfile.txt", "a+")
 
     # write some lines of data to the file
     for i in range(10):
