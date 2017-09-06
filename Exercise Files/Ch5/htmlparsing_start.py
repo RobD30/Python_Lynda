22c16782ed80efe807ad03ec2f2e54916a5b8198
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 21:56:50 -0700

    Refactor: parsed for comment
    end_of_line

--- Exercise Files/Ch5/htmlparsing_start.py
+++ Exercise Files/Ch5/htmlparsing_start.py
@@ -1,13 +1,27 @@
-# 
-# Example file for parsing and processing HTML
-# (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
+#!/usr/bin/env python3
+
+from html.parser import HTMLParser
+
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    # function to handle the processing of HTML comments
+    def handle_comment(self, data):
+        print("Encountered comment:", data)
+        pos = self.getpos()
+        print("At line: ", pos[0], " position ", pos[1])
 
 
 def main():
-  # instantiate the parser and feed it some HTML
-  parser = MyHTMLParser()
-    
+    # instantiate the parser and feed it some HTML
+    parser = MyHTMLParser()
+
+    # open the sample HTML file and read it
+    f = open("samplehtml.html")
+    if f.mode == "r":
+        contents = f.read()  # read the entire file
+        parser.feed(contents)
+
 
 if __name__ == "__main__":
-  main();
-  
+    main();
