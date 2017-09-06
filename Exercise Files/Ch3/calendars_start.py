91e651f6ea825d7120029271751e1366ad3f6b75
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 20:57:11 -0700

    Refactor: Finished Chapter 3.
    end_of_line

--- Exercise Files/Ch3/calendars_start.py
+++ Exercise Files/Ch3/calendars_start.py
@@ -6,17 +6,17 @@ import calendar
 def main():
     # create a plain text calendar
     c = calendar.TextCalendar(calendar.SUNDAY)
-    str = c.formatmonth(2013, 1, 0, 0)
+    str = c.formatmonth(2017, 1, 0, 0)
     print(str)
 
     # create an HTML formatted calendar
     hc = calendar.HTMLCalendar(calendar.SUNDAY)
-    str = hc.formatmonth(2013, 1)
+    str = hc.formatmonth(2017, 1)
     print(str)
 
     # loop over the days of a month
     # zeroes mean that the day of the week is in an overlapping month
-    for i in c.itermonthdays(2013, 8):
+    for i in c.itermonthdays(2017, 8):
         print(i)
 
     # The Calendar module provides useful utilities for the given locale,
@@ -33,7 +33,7 @@ def main():
     # we can use this script:
     for m in range(1, 13):
         # returns an array of weeks that represent the month
-        cal = calendar.monthcalendar(2013, m)
+        cal = calendar.monthcalendar(2017, m)
         # The first Friday has to be within the first two weeks
         weekone = cal[0]
         weektwo = cal[1]
